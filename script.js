4faabbbd7ced3e3a00163bf920ea610994e452dd
Aliya Gamez <aliya.e.gamez@gmail.com>
2025-09-01 21:06:53 -0400

    Added initial files, some functions, and created basic HTML/CSS calculator that will be styled further in the future

---
+++ script.js
@@ -0,0 +1,27 @@
+// Variables
+
+let num1;
+let num2;
+let operator;
+
+// Functions: Basic Math Operations
+
+function addCalc(a,b) {
+    return a + b;
+}
+
+function subtractCalc(a,b) {
+    return a - b;
+}
+
+function multiplyCalc(a,b) {
+    return a * b;
+}
+
+function divideCalc(a,b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    
+}
